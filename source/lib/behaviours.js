64074c6acbe5d8228c114d1748038156d88fa1d2
dudematthew <mmoczydlowski4@gmail.com>
2022-03-16 14:02:26 +0100

    Update .gitignore; Add lib

---
+++ source/lib/behaviours.js
@@ -0,0 +1,14 @@
+import $ from "jquery";
+
+/**
+ * Hide navbar dropdowns if clicked outside of them
+ */
+$(window).on("click", function() {
+    $(document).on("click", function(event) {
+        $(".dropdown").each(function () {
+            if (!$.contains($(this).get(0), $(event.target).get(0))) {
+                $(this).toggleClass("is-active", false);
+            }
+        });
+    });
+});
---
+++ source/lib/settings.js
@@ -0,0 +1,14 @@
+let iconPickerOptions = {
+  iconSets: [ {
+    name: 'simpleLine', // Name displayed on tab
+    css: 'https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css', // CSS url containing icons rules
+    prefix: 'icon-', // CSS rules prefix to identify icons
+    displayPrefix: ''
+  },
+  {
+    name: 'fontAwesome', // Name displayed on tab
+    css: 'https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css', // CSS url containing icons rules
+    prefix: 'fa-', // CSS rules prefix to identify icons
+    displayPrefix: 'fa fa-icon'
+  } ]
+};
